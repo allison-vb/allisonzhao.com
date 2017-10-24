dea8f0740c58ae092ab5d8ef135a57c596930f48
Allison von Bargen <gryffinzhao@gmail.com>
2017-10-24 15:46:12 -0400

    minor edit

--- skin-care-helper/react/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer () {
-        return (
-            <footer>
-                <i className="fa fa-home fa-3x" aria-hidden="true"></i>
-            </footer>
-        )
-}
